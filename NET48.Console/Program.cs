de684852744f68387517d40c8d0bb4264975a745
Viktor Rosvall <viktor.rosvall@monitor.se>
2024-07-24 09:45:28 +0800

    Initial commit

---
+++ NET48.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NET48.Console
+{
+	internal class Program
+	{
+		public static void Main(string[] args)
+		{
+			System.Console.WriteLine("Hello World!");
+			System.Console.WriteLine("You should have seen a UAC prompt if the manifest was loaded!");
+			System.Console.ReadLine();
+		}
+	}
+}
---
+++ NET8.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace NET8.Console;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		System.Console.WriteLine("Hello World!");
+		System.Console.WriteLine("You should have seen a UAC prompt if the manifest was loaded!");
+		System.Console.ReadLine();
+	}
+}
